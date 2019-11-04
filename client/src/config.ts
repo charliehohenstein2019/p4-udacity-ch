b2fca494f63fb81a37c4f3e139670b0efaa3098e
charliehohenstein2019 <chohenst@asu.edu>
2019-11-03 22:47:51 -0600

    back and front end working

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wvzowynl0b';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'charlie-1996.auth0.com',
+  clientId: 'x94ZyO63pNSn4GfTvWFbKqoJUWmfqTJ2',
+  callbackUrl: 'http://localhost:3000/callback'
+};
